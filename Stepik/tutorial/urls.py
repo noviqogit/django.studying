0ba62aaf2a00d492196e30f36aa958ec5f7c8953
sasha novikov <novikov.ae@gmail.com>
2023-01-16 17:06:03 +0300

    include URLconf of app in project

--- Stepik/tutorial/urls.py
+++ Stepik/tutorial/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
 from tutorial import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('respose', views.func),
-    path('resource/', include('tutorial.urls')),
+    path('one', views.func),
 ]
