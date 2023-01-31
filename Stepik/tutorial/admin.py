26f1e585ed54792aa10396643ad3f250c66e6a65
sasha novikov <novikov.ae@gmail.com>
2023-01-31 21:41:00 +0300

    admin annotation row

--- Stepik/tutorial/admin.py
+++ Stepik/tutorial/admin.py
@@ -7,6 +7,7 @@ class TableAdmin(admin.ModelAdmin):
     list_display = ['char_column',  # первое значение является ссылочным, нельзя редактировать
                     'int_column',
                     'int_column2',
+                    'annotaton'
                     ]
     list_editable = ['int_column',
                      'int_column2',
@@ -15,6 +16,14 @@ class TableAdmin(admin.ModelAdmin):
                 'int_column2',
                 ]
     list_per_page = 5
+
+    @admin.display(ordering='int_column', description='status')
+    def annotaton(self, row: Table):
+        if not row.int_column2:
+            return 'null'
+        if row.int_column2 < 20:
+            return '< 20'
+        return '>= 20'
 
 
 # Register your models here.
